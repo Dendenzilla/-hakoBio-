c6573c0f640e0d72c7829f5360527a8ec355d579
Dendenzilla <martinadriaensen@gmail.com>
2019-03-20 13:10:38 +0100

    Chat v0.8

--- src/components/Chatbot.vue
+++ src/components/Chatbot.vue
@@ -1,48 +1,29 @@
 <template>
   <div class="chatbot">
     <div class="hako_title">
-      <p>Hako</p>
-      <p>T</p>
+      <p> HAKO</p>
     </div>
     <div class="robotHead">
       <img src="../assets/hako-happy.png">
     </div>
     <div class="chat">
-      <div class="bubble bot">
-        <p>OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
-      </div>
-      <div class="bubble bot">
-        <p>Une phrase corecte pour voir si ça se met nbien</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
-      </div>
-      <div class="bubble user">
-        <p>Lorem ipsum dolor sit</p>
+      <div v-for="(c,index) in chatlog" :key="index" class="tdiv" v-show="c.text">
+          <div v-if="c.type === 'R'" >
+            <div class="bubble bot">
+              <p>{{c.text}}</p>
+            </div>
+          </div>
+          <div v-else class="tdiv">
+            <div class="bubble user">
+              <p>{{c.text}}</p>
+            </div>
+          </div>
       </div>
     </div>
     <div class="mouth">
       <form action>
-        <textarea rows="3" v-model="userQ" placeholder="Type your question here"></textarea>
-        <button @click.prevent="post()">Ask Hako</button>
+        <textarea rows="3" v-model="question" placeholder="Type your question here"></textarea>
+        <button @click.prevent="post()">Ask <br>Hako</button>
       </form>
     </div>
   </div>
@@ -51,47 +32,34 @@
 <script>
 import axios from "axios";
 
-/*export default {
+export default {
     data() {
         return {
-            userQ: 'hakobio',
-            questions: []
+          chatlog: [{
+            text:`Hi I'm Hako ! Can I help you ?'`,
+            type:'R'
+          }
+          ],
+          question: ''
         }
     },
     name:'Chatbot',
     methods: {
-        post() {
-            console.log('test')
+        post() { 
+          this.chatlog.push({text:this.question, type:'Q'});
             var headers = {
                 'Content-Type': 'application/json',
                 'Authorization': 'EndpointKey afdff838-d95c-40d3-a32d-b0488a1ee7e5' 
             }
-            axios.post("https://qnabxl.azurewebsites.net/qnamaker/knowledgebases/09ae110d-19ea-407c-bbea-0c4cdf8383f3/generateAnswer", {"questions":"hakobio"} ,{headers: headers} ).then(response => {
-                this.questions = response.data;
-                console.log('test2')
+            axios.post("https://qnabxl.azurewebsites.net/qnamaker/knowledgebases/09ae110d-19ea-407c-bbea-0c4cdf8383f3/generateAnswer", {"question":this.question} ,{headers: headers} ).then(response => {
+                let botAnswer = response.data;
+                this.question = '';
+                this.chatlog.push({text:botAnswer.answers[0].answer, type:'R'});
             })
         }
     }
-}*/
-
-export default {
-    data() {
-        return {
-            userQ: '',
-            questions: ''
-        }
-    },
-    name:'Chatbot',
-    created() {
-        var headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'EndpointKey afdff838-d95c-40d3-a32d-b0488a1ee7e5' 
-        }
-        axios.post("https://qnabxl.azurewebsites.net/qnamaker/knowledgebases/09ae110d-19ea-407c-bbea-0c4cdf8383f3/generateAnswer",{"question":"What is hakobio"} ,{headers: headers} ).then(response => {
-            this.questions = response.data;
-        })
-    }
 }
+
 
 </script>
 
@@ -104,12 +72,18 @@ export default {
   border-radius: 6px 6px 0 0;
   background-color: #d3d7d8;
 }
+.tdiv {
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  width: 100%;
+}
 .robotHead {
   position: absolute;
   top: -35px;
-  left: 6px;
+  left: 16px;
   height: 60px;
-  width: 60px;
+  width: 65px;
   & img {
     width: 100%;
   }
@@ -126,10 +100,7 @@ export default {
 }
 .chat {
   overflow-y: scroll;
-  max-height: 400px;
-  display: flex;
-  flex-direction: column;
-  align-items: baseline;
+  height: 325px;
   padding: 0 12px;
   width: 276px;
   border-top: 2px solid rgb(208, 210, 211);
@@ -158,14 +129,40 @@ export default {
 .chat p {
   margin: 0;
 }
+form{
+  display: flex;
+  justify-content: center;
+}
 textarea {
-  width: 90%;
+  width: 70%;
   resize: none;
   font-family: Montserrat;
+  border-color:transparent;
+  border-radius: 15px 0 0 15px;
+  padding:5px;
+  // box-shadow: 3px 5px 15px #e0e6e8 inset;
 }
+
+textarea:focus{
+  outline-color: #00b3b3;
+}
+
+button{
+  color: #fff;
+  background:#0b1927;
+  border-radius: 0 15px 15px 0;
+  border: 1px solid #0b1927;
+  cursor: pointer;
+}
+button:hover{
+  border:1px solid #00b3b3;
+  background-color:#00b3b3;
+}
+
 .mouth {
-  bottom: 12px;
-  width: 100%;
+  position:fixed;
+  top: 423px;
+  width: 300px;
   border-top: 2px solid rgb(208, 210, 211);
   padding: 12px 0;
   background-color: #e0e6e8;
